3e900d147b33714a9d307a5f1541820ef2213561
sasha <sashkavalent@gmail.com>
2014-11-06 22:17:34 +0300

    Delete commented lines.

--- area_allocation/support.rb
+++ area_allocation/support.rb
@@ -1,32 +1,5 @@
 MAX_INT = 256 * 256 - 1
 
-# class Magick::Image::View
-#   def each(&block)
-#     for x in (0...self.width) do
-#       for y in (0...self.height) do
-#         yield(self[y][x], x, y)
-#       end
-#     end
-#   end
-
-#   def map(&block)
-#     result = []
-#     each { |pix| result << yield(pix) }
-#     result
-#   end
-
-#   def group_by(&block)
-#     result = {}
-#     each do |pix|
-#       key = yield(pix)
-#       result[key] ||= []
-#       result[key] << pix
-#     end
-#     result
-#   end
-
-# end
-
 class Magick::Pixel
   attr_accessor :color_label, :area_number, :x, :y
   def inspect
